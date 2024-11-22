8994bf6116fdf89b48f0e30886e6b952f837a740
Kvasnitsyn Denis <kvasnicyn@gmail.com>
2024-11-22 22:43:16 +0500

    start

---
+++ src/test/java/ru/pet_project/weather_app/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet_project.weather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
